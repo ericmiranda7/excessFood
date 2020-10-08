94800552f78bb9c32766104bc84430b5d2f54780
Eric <ericmiranda7@gmail.com>
2020-10-08 20:59:29 +0530

    set navbar size

--- excessFood/urls.py
+++ excessFood/urls.py
@@ -21,7 +21,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('accounts/', include('accounts.urls'))
