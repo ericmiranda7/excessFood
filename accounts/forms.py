3e4d86b2b88947ae781ceb1877baff78aabdd883
Manish Bharti <mbharti321@gmail.com>
2020-10-15 22:06:30 +0530

    style profile form and update forms.py accordingly

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,9 +4,29 @@ from django.forms.models import ModelForm
 from django.db import models
 from django import forms
 
+
+
+
+# class DonatorForm(ModelForm):
+#     user = forms.IntegerField(widget=forms.HiddenInput())
+#     class Meta:
+#         model = Donator
+#         fields = '__all__'
+    
+
+
+
 class DonatorForm(ModelForm):
     user = forms.IntegerField(widget=forms.HiddenInput())
     class Meta:
         model = Donator
-        fields = '__all__'
-    
+        # fields = '__all__'
+        fields = ('first_name', 'last_name', 'email', 'phone', 'addresses')
+
+        widgets = {
+        			'first_name': forms.TextInput(attrs = {'class': 'form-control'}),
+        			'last_name': forms.TextInput(attrs = {'class': 'form-control'}),
+        			'email': forms.TextInput(attrs = {'class': 'form-control'}),
+        			'phone': forms.TextInput(attrs = {'class': 'form-control'}),
+        			'addresses': forms.Select(attrs = {'class': 'form-control'}),
+        }
