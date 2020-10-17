ff53a981dd302bfe6e1e854c0749c92a7b8cde14
Eric <ericmiranda7@gmail.com>
2020-10-17 15:59:17 +0530

    add points page

--- excessFood/settings.py
+++ excessFood/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django.contrib.gis',
+    'mathfilters',
     'smart_selects',
     'accounts',
     'food',
